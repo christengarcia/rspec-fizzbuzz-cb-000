066587cf9daf5d6bc36864439937d2a3a2c3ec67
Christen Garcia <christen.garcia@gmail.com>
2017-10-25 03:36:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
-def fizzbuzz(num)
-  if num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  elsif num % 3 = 0 && num % 5 = 0
-    return "FizzBuzz"
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 = 0 && int % 5 = 0
+    "FizzBuzz"
   else
     return "nil"
   end
